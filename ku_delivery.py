23da1185f820ad708b46b7271689037e3a24cf0e
Feux Follets <phirawitsommai@gmail.com>
2025-12-04 16:00:19 +0700

    Added comments for better readability

--- ku_delivery.py
+++ ku_delivery.py
@@ -1,3 +1,5 @@
+# All Classes
+
 class Person():
     def __init__(self, name):
         self.name = name
@@ -38,8 +40,12 @@ class DeliveryOrder():
     def summary(self):
         return f"Order Summary:\nItem: {self.item}\nCustomer: {self.customer}\nStatus: {self.status}\nDriver: {self.driver}"
 
+# Main Program
+
+# Creates an empty list to track all orders
 all_orders = []
 
+# Creates two customers and one driver
 customer1 = Customer("Alice", "32nd Good Wood Road")
 customer2 = Customer("Bob", "Yellow Street")
 driver1 = Driver("David", "Motorcycle")
@@ -48,21 +54,25 @@ customer2.introduce()
 driver1.introduce()
 print()
 
+# Place the first order
 order1 = customer1.place_order("Laptop")
 order1.assign_driver(driver1)
 all_orders.append(order1)
 print(order1.summary())
 print()
 
+# Place the second order
 order2 = customer2.place_order("Headphones")
 order2.assign_driver(driver1)
 all_orders.append(order2)
 print(order2.summary())
 print()
 
+# Deliver all the orders
 driver1.deliver(order1)
 driver1.deliver(order2)
 
+# Print out the final status
 print()
 print("Final Status:")
 for i in all_orders:
